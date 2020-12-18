9eea5ffd5ffee207de8c339ab90db69877632cc9
Krittipong <krittipong@ava.fund>
2020-12-18 09:18:30 +0700

    bug

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 airflow db init
-airflow users --create -e krittipong@ava.fund -f Krittipong -l Kanchanapiboon -p avaalpha -r Admin -u yort
+airflow users create -e krittipong@ava.fund -f Krittipong -l Kanchanapiboon -p avaalpha -r Admin -u yort
 airflow webserver
