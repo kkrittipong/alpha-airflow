0abb70c8d0aa8c26a24f0bb8f7c151fc058a066b
Krittipong <krittipong@ava.fund>
2020-12-18 09:24:03 +0700

    run in background

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 airflow db init
 airflow users create -e krittipong@ava.fund -f Krittipong -l Kanchanapiboon -p avaalpha -r Admin -u yort
-airflow webserver
-airflow scheduler
+airflow webserver -D
+airflow scheduler -D
