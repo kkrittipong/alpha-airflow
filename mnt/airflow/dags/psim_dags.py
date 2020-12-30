5b02fc1c3f85227b5e19e7ef24e5de1f75e01073
krittipong <krittipong@ava.fund>
2020-12-30 22:20:38 +0700

    update schedue interval

--- mnt/airflow/dags/psim_dags.py
+++ mnt/airflow/dags/psim_dags.py
@@ -122,7 +122,7 @@ def extract_psim_loopnum(token, group, file):
 
 
     
-@dag(default_args=default_args, schedule_interval='00 10 * * *', start_date=days_ago(5))
+@dag(default_args=default_args, schedule_interval='00 23 * * *', start_date=days_ago(5))
 def set_psim_etl():
     """
     ### TaskFlow API Tutorial Documentation
